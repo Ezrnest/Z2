314dff7c0ee63ee6df714ed92a033da7de2206d9
chenzc18 <chenzc18@mails.tsinghua.edu.cn>
2019-08-28 17:56:57 +0800

    Merge branch 'master' of D:\Z2 with conflicts.

--- qt/Z2/main.cpp
+++ qt/Z2/main.cpp
@@ -2,7 +2,7 @@
 #define ASIO_STANDALONE
 
 #include "gamewindow.h"
-#include "imagerepository.h"
+#include "ImageRepository.h"
 #include "SoundRepository.h"
 #include "mainwindow.h"
 #include <QApplication>
@@ -12,34 +12,20 @@
 #include "entity/ConstructionBase.h"
 #include "core/Server.h"
 #include "core/LocalClient.h"
-#include "bot/BotClientProxy.h"
-#include <QTranslator>
+#include "bot/BotClientPort.h"
 using namespace z2;
 
-static QTranslator* trans = nullptr;
-void initLang(QApplication& app){
-    auto& config =  z2::GameConfiguration::instance();
-    string lang = config.getProp().get("language","cn");
-    trans = new QTranslator;
-    if(lang == "cn"){
-        trans->load(":/lang_cn.qm");
-    }else{
-        cout << "Loading en:" << trans->load(":/lang_en.qm") << endl;
-    }
-    app.installTranslator(trans);
-}
 
-void initAboutQT(QApplication& app){
-    initLang(app);
+void initAboutQT(){
     auto resDir = z2::GameConfiguration::getResourceDir();
     z2::ImageRepository::instance().initFromFolder(resDir.subFile("image"));
     z2::SoundRepository::instance().initFromFolder(resDir.subFile("sound"));
     qRegisterMetaType<shared_ptr<GameEvent>>("shared_ptr<GameEvent>");
 }
 
-void initAll(QApplication& app){
+void initAll(){
     z2::GameConfiguration::initAll();
-    initAboutQT(app);
+    initAboutQT();
 }
 
 shared_ptr<z2::World> getWorld(){
@@ -48,7 +34,7 @@ shared_ptr<z2::World> getWorld(){
     //        w->createEntity(Point(0, 1), "Fighter", 0);
     //        w->createEntity(Point(1, 1), "Farmer", 0);
     w->createEntity(Point(3, 3), "Farmer", 1);
-    w->getTile(0, 3).setResource(Resource::MINE);
+    w->getTile(0,3).setResource(Resource::MINE);
     w->getTile(3,4).setResource(Resource::GEM);
     w->getTile(2,0).setTerrain(Terrain::MOUNTAIN);
     w->getTile(2,1).setTerrain(Terrain::MOUNTAIN);
@@ -67,7 +53,8 @@ void buildTestGame(shared_ptr<Server>& server, shared_ptr<LocalClient>& client){
     local->setRealServer(server);
     server->registerClient(local);
     client = local;
-    shared_ptr<BotClientProxy> bot(new BotClientProxy());
+
+    shared_ptr<BotClientPort> bot(new BotClientPort());
     bot->setServer(server);
     server->registerClient(bot);
 
@@ -98,8 +85,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    initAll(a);
+    initAll();
 
-    return runMainMenu(a);
+    return runGameWindow(a);
+//    return runMainMenu(a);
 
 }
